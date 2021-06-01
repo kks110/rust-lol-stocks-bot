6dafe02a53c938011010613762339f7a2c6392b2
Kelvin Samuel <kelvin@kks110.com>
2021-06-01 17:08:55 +0100

    Update register.rs

--- src/commands/register.rs
+++ src/commands/register.rs
@@ -1,6 +1,3 @@
-use crate::models::users::User;
-use crate::file_io::users::register_user;
-
 use serenity::prelude::*;
 use serenity::model::prelude::*;
 use serenity::framework::standard::{
@@ -8,21 +5,15 @@ use serenity::framework::standard::{
     macros::command,
 };
 
+use crate::database::connection::establish_connection;
+use crate::database::users::create_user;
+
 #[command]
 pub async fn register(ctx: &Context, msg: &Message) -> CommandResult {
-    let user = User {
-        username: msg.author.name.to_owned(),
-        balance: 2000.to_owned(),
-        portfolio: Vec::new(),
-    };
+    let conn = establish_connection();
+    let new_user = create_user(&conn, &msg.author.name);
 
-    match register_user(user) {
-        Err(e) => { msg.channel_id.say(&ctx.http, e).await?; }
-        Ok(_) => {
-            let mut response = msg.author.name.clone();
-            response.push_str( " has been registered.");
-            msg.channel_id.say(&ctx.http, response).await?;
-        }
-    }
+    let response = format!("Updated user {}. Starting Balance is {}", new_user.name, new_user.balance);
+    msg.channel_id.say(&ctx.http, response).await?;
     Ok(())
 }
