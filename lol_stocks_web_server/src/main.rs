3e19040b1b1a0c8fac0d6ebfa77846af237d3afb
Kelvin Samuel <kelvin@kks110.com>
2021-06-09 00:13:28 +0100

    add a bind on 0.0.0.0 so it can be accessed

--- lol_stocks_web_server/src/main.rs
+++ lol_stocks_web_server/src/main.rs
@@ -43,7 +43,8 @@ async fn main() -> std::io::Result<()> {
             .service(register_matches)
             .service(padlock)
     })
-        .bind("127.0.0.1:8080")?
+        .bind("127.0.0.1:8181")?
+        .bind("0.0.0.0:8080")?
         .run()
         .await
 }
