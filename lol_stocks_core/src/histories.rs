d6fd9df8b35179a73a03abb499b4102091b1994f
Kelvin Samuel <kelvin@kks110.com>
2021-06-10 17:28:01 +0100

    change history to use newly extracted calculate_portfolio_value

--- lol_stocks_core/src/histories.rs
+++ lol_stocks_core/src/histories.rs
@@ -1,16 +1,13 @@
 use crate::database::{
     connection::establish_connection,
-    teams::{ load_teams, load_team_by_id },
+    teams::load_teams,
     team_elo_histories::create_team_elo_history,
     users::load_users,
     user_portfolio_histories::create_user_portfolio_history,
     portfolios::load_users_portfolio,
 };
 
-use crate::models::{
-    user::User,
-    portfolio::Portfolio
-};
+use crate::portfolio_calculations::calculate_portfolio_value;
 
 pub fn take_history_snapshot() {
     update_team_history();
@@ -22,30 +19,19 @@ fn update_team_history() {
 
     let teams = load_teams(&conn);
     for team in teams {
-        create_team_elo_history(&conn, &team.elo, &team.id)
+        create_team_elo_history(&conn, &team.elo, &team.id);
     }
 }
 
 fn update_user_history() {
+    println!("updating users");
     let conn = establish_connection();
 
     let users = load_users(&conn);
     for user in users {
         let portfolio = load_users_portfolio(&conn, &user);
-        let portfolio_value = calculate_portfolio_value(&user, &portfolio);
+        let portfolio_value = calculate_portfolio_value(&conn, &user, &portfolio);
 
-        create_user_portfolio_history(&conn, &portfolio_value, &user.id)
+        create_user_portfolio_history(&conn, &portfolio_value, &user.id);
     }
-}
-
-fn calculate_portfolio_value(user: &User, portfolio: &Vec<Portfolio>) -> i32 {
-    let mut value = 0;
-    value = value + user.balance;
-
-    for holding in portfolio {
-        let team = load_team_by_id(&conn, &holding.team_id);
-        let holding_value = team.elo * holding.amount;
-        value = value + holding_value;
-    }
-    value
 }
