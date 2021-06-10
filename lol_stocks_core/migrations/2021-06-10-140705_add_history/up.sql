1c114a63ab3967fb9d651a6d77c02e78ca83c3c7
Kelvin Samuel <kelvin@kks110.com>
2021-06-10 17:26:31 +0100

    change sql to register as yesterdays date

--- lol_stocks_core/migrations/2021-06-10-140705_add_history/up.sql
+++ lol_stocks_core/migrations/2021-06-10-140705_add_history/up.sql
@@ -1,14 +1,14 @@
 -- Your SQL goes here
 CREATE TABLE team_elo_histories (
     id SERIAL PRIMARY KEY,
-    date DATE NOT NULL DEFAULT CURRENT_DATE,
+    date DATE NOT NULL DEFAULT CURRENT_DATE - INTEGER '1',
     elo INTEGER NOT NULL,
     team_id INTEGER NOT NULL references teams(id)
 );
 
 CREATE TABLE user_portfolio_histories (
     id SERIAL PRIMARY KEY,
-    date DATE NOT NULL DEFAULT CURRENT_DATE,
+    date DATE NOT NULL DEFAULT CURRENT_DATE- INTEGER '1',
     value INTEGER NOT NULL,
     user_id INTEGER NOT NULL references users(id)
 );
