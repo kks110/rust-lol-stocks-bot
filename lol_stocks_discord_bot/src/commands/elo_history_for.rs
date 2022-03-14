50a44827a942d2bd83f8320ba556559ed191de60
Kelvin Samuel <kelvin@kks110.com>
2022-03-14 11:20:36 +0000

    update elo history for response

--- lol_stocks_discord_bot/src/commands/elo_history_for.rs
+++ lol_stocks_discord_bot/src/commands/elo_history_for.rs
@@ -8,31 +8,58 @@ use serenity::framework::standard::{
 
 use std::error::Error;
 use std::result::Result;
+use chrono::{NaiveDate, Utc};
 
 use lol_stocks_core::database::{
     connection::establish_connection,
     teams::load_team,
     team_elo_histories::load_team_elo_history,
 };
+use crate::helpers::plus_sign::plus_sign;
+
+struct HistoryData {
+    pub date: NaiveDate,
+    pub value: i32,
+    pub difference: i32
+}
 
 #[command]
 pub async fn elo_history_for(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
-    let response: String;
+    let mut entries: Vec<HistoryData> = vec![];
+    let mut error_occurred: Option<String> = None;
+    let mut team_name: String = "".to_string();
 
     match parse_args(args) {
-        Ok(t) => {
-            let team_name = t;
-            match load_elo_history(&team_name) {
-                Ok(message) => { response = message },
-                Err(e) => { response = format!("An error has occurred: {}", e.to_string())}
-            }
+        Ok(name) => { team_name = name },
+        Err(e) => { error_occurred = Some(e.to_string()) }
+    };
 
-        },
-        Err(e) => { response = format!("An error as occurred {}", e.to_string()); }
+
+    match load_elo_history(&team_name) {
+        Ok(message) => { entries = message },
+        Err(e) => { error_occurred = Some(e.to_string()) }
+    }
+
+    if error_occurred.is_some() {
+        msg.channel_id.say(
+            &ctx.http,
+            format!("An Error as occurred: {}", error_occurred.unwrap().to_string())
+        ).await?;
+        return Ok(())
     }
 
 
-    msg.channel_id.say(&ctx.http, response).await?;
+    msg.channel_id.send_message(&ctx.http, |m| {
+        m.embed(|e| {
+            let mut response = vec![];
+            for entry in entries {
+                response.push((format!("{}", entry.date), format!("{} ({}{})", entry.value, plus_sign(entry.difference), entry.difference), false ))
+            };
+            e
+                .title(format!("{}'s performance", team_name.to_uppercase()))
+                .fields(response)
+        })
+    }).await?;
     Ok(())
 }
 
@@ -40,16 +67,34 @@ fn parse_args(mut args: Args) -> Result<String, Box<dyn Error>> {
     Ok(args.single::<String>()?)
 }
 
-fn load_elo_history(team_name: &str) -> Result<String, Box<dyn Error>> {
+fn load_elo_history(team_name: &str) -> Result<Vec<HistoryData>, Box<dyn Error>> {
     let conn = establish_connection();
     let team = load_team(&conn, team_name)?;
     let team_elo_history = load_team_elo_history(&conn, &team)?;
 
-    let mut message = format!("Date: Now, Value: {}\n", team.elo);
+    let mut history_data: Vec<HistoryData> = vec![
+        HistoryData{
+            date: Utc::now().date().naive_utc(),
+            value: team.elo,
+            difference: team.elo - team_elo_history.first().unwrap().elo
+        }
+    ];
 
-    for entry in team_elo_history {
-        let response_line = format!("Team: {}, Date: {}, Price: {}\n", team.name, entry.date, entry.elo);
-        message.push_str(&response_line)
+    let mut counter = 1;
+
+    for entry in &team_elo_history {
+        let previous_value = match team_elo_history.get(counter) {
+            Some(history) => history.elo,
+            None => entry.elo
+        };
+
+        history_data.push(HistoryData{
+            date: entry.date,
+            value: entry.elo,
+            difference: entry.elo - previous_value
+        });
+        counter += 1;
     }
-    Ok(message)
+
+    Ok(history_data)
 }
