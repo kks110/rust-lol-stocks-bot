f59035b58abf7e60090c6bcf3922281102593bae
Kelvin Samuel <kelvin@kks110.com>
2022-03-19 22:28:15 +0000

    add methods for sending images

--- lol_stocks_discord_bot/src/helpers/messages.rs
+++ lol_stocks_discord_bot/src/helpers/messages.rs
@@ -40,3 +40,41 @@ pub async fn send_error_message<T: Display>(ctx: &Context, msg: &Message, error:
     }).await?;
     Ok(())
 }
+
+pub async fn send_image_message(ctx: &Context, msg: &Message, image_location: String) -> CommandResult {
+    msg.channel_id.send_message(&ctx.http, |m| {
+        m.add_file(&image_location[..])
+    }).await?;
+    Ok(())
+}
+
+pub async fn send_image_as_attachment<T: Display, S: Display + Into<String>>(
+    ctx: &Context,
+    msg: &Message,
+    title: T,
+    description: Option<T>,
+    fields: Option<Vec<(S, S, bool)>>,
+    attachment: &str,
+) -> CommandResult {
+    let file_location = format!("./images/{}", &attachment);
+
+    msg.channel_id.send_message(&ctx.http, |m| {
+        m.embed(|e| {
+            e.colour(0x4287f5);
+            e.title(title);
+
+            if description.is_some() {
+                e.description(description.unwrap());
+            }
+
+            if fields.is_some() {
+                e.fields(fields.unwrap());
+            }
+            e.thumbnail(format!("attachment://{}", attachment))
+        });
+
+        m.add_file(&file_location[..]);
+        m
+    }).await?;
+    Ok(())
+}
